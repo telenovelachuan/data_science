b83f90e59cad25df483db1ab73a474ca6d2e6e05
telenovelachuan <telenovelachuan@gmail.com>
2020-09-02 09:36:56 -0500

    initializing 690

---
+++ EECS_690_intro_ml/assignment1/NBClassifier.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv(url, names=names)
+
+
