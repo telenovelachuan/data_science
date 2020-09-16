0e85bc5733fe83466f4ff828aaf0378b67787c1d
telenovelachuan <telenovelachuan@gmail.com>
2020-09-15 21:11:55 -0500

    updating test cases

--- EECS_662_programming_language/a0.hs
+++ EECS_662_programming_language/a0.hs
@@ -195,9 +195,17 @@ evalM (Mult l r) = do {l' <- (evalM l);
                        Just (liftNum (*) l' r')}
 evalM (Div l r) = do {l' <- (evalM l);
                       r' <- (evalM r);
-                      if r' == 0 then Nothing else return (l' `div` r')}   
+                      if r' == 0 then Nothing else return (l' `div` r')}  
+evalM (If0 x y z) = do {x' <- (evalM x);
+                        y' <- (evalM y);
+                        z' <- (evalM z);
+                        case x' of
+                          0 -> case y' of 
+                                      0 -> Just z'
+                                      _ -> Just y'
+                          _ -> Just x'} 
                    
-evalM _ = Nothing
+--evalM _ = Nothing
 
 --evalM test cases
 evalM_t1 = evalM (pos1)
@@ -213,9 +221,96 @@ evalM_t10 = evalM (Mult pos2 zero)
 evalM_t11 = evalM (Div pos1 pos2)
 evalM_t12 = evalM (Div pos1 neg1)
 evalM_t13 = evalM (Div pos1 zero)
+evalM_t14 = evalM (If0 pos1 pos2 pos3)
+evalM_t15 = evalM (If0 zero pos2 pos3)
+evalM_t16 = evalM (If0 zero neg1 pos3)
+evalM_t17 = evalM (If0 zero zero pos3)
+evalM_t18 = evalM (If0 zero zero zero)
 
 interpAE :: String -> Maybe Int
-interpAE _ = Nothing
+interpAE str = evalM (parseAE str)
+--interpAE _ = Nothing
+
+--interpAE test cases
+interpAEt1 = interpAE "1+3"
+interpAEt2 = interpAE "(1+3)*5 + 1"
+interpAEt3 = interpAE "(1+3)*5/0"
+interpAEt4 = interpAE "(1+3)*(-1)/8"
+interpAEt5 = interpAE "(1+3)*2-100"
+
+
+run_test_cases = do
+            putStrLn "Running test cases..."
+            print "evalAE test cases"
+            print ("evalAE (Num 10): ", evalAE_t1)
+            --print ("evalAE (Num -3): ", evalAE_t2)
+            print ("evalAE Plus ((Num 10) (Num 2)): ", evalAE_t3)
+            --print ("evalAE Plus ((Num 10) (Num -3)): ", evalAE_t4)
+            print ("evalAE Minus ((Num 10) (Num 2)): ", evalAE_t5)
+            --print ("evalAE Minus ((Num 2) (Num 10)): ", evalAE_t6)
+            --print ("evalAE Minus ((Num 10) (Num -3)): ", evalAE_t7)
+            print ("evalAE Mult ((Num 10) (Num 2)): ", evalAE_t8)
+            --print ("evalAE Mult ((Num 10) (Num -3)): ", evalAE_t9)
+            print ("evalAE Mult ((Num 10) (Num 0)): ", evalAE_t10)
+            print ("evalAE Div ((Num 10) (Num 2)): ", evalAE_t11)
+            --print ("evalAE Div ((Num 10) (Num -3)): ", evalAE_t12)
+            --print ("evalAE Div ((Num 10) (Num 0)): ", evalAE_t13)
+            print ("evalAE If0 ((Num 10) (Num 2) (Num 4)): ", evalAE_t14)
+            print ("evalAE If0 ((Num 0) (Num 2) (Num 4)): ", evalAE_t15)
+            --print ("evalAE If0 ((Num 0) (Num -3) (Num 4)): ", evalAE_t16)
+            print ("evalAE If0 ((Num 0) (Num 0) (Num 4)): ", evalAE_t17)
+            print ("evalAE If0 ((Num 0) (Num 0) (Num 0)): ", evalAE_t18)
+
+
+            print "evalAEMaybe test cases"
+            print ("evalAEMaybe (Num 10): ", evalAEMaybe_t1)
+            print ("evalAEMaybe (Num -3): ", evalAEMaybe_t2)
+            print ("evalAEMaybe Plus ((Num 10) (Num 2)): ", evalAEMaybe_t3)
+            print ("evalAEMaybe Plus ((Num 10) (Num -3)): ", evalAEMaybe_t4)
+            print ("evalAEMaybe Minus ((Num 10) (Num 2)): ", evalAEMaybe_t5)
+            print ("evalAEMaybe Minus ((Num 2) (Num 10)): ", evalAEMaybe_t6)
+            print ("evalAEMaybe Minus ((Num 10) (Num -3)): ", evalAEMaybe_t7)
+            print ("evalAEMaybe Mult ((Num 10) (Num 2)): ", evalAEMaybe_t8)
+            print ("evalAEMaybe Mult ((Num 10) (Num -3)): ", evalAEMaybe_t9)
+            print ("evalAEMaybe Mult ((Num 10) (Num 0)): ", evalAEMaybe_t10)
+            print ("evalAEMaybe Div ((Num 10) (Num 2)): ", evalAEMaybe_t11)
+            print ("evalAEMaybe Div ((Num 10) (Num -3)): ", evalAEMaybe_t12)
+            print ("evalAEMaybe Div ((Num 10) (Num 0)): ", evalAEMaybe_t13)
+            print ("evalAEMaybe If0 ((Num 10) (Num 2) (Num 4)): ", evalAEMaybe_t14)
+            print ("evalAEMaybe If0 ((Num 0) (Num 2) (Num 4)): ", evalAEMaybe_t15)
+            print ("evalAEMaybe If0 ((Num 0) (Num -3) (Num 4)): ", evalAEMaybe_t16)
+            print ("evalAEMaybe If0 ((Num 0) (Num 0) (Num 4)): ", evalAEMaybe_t17)
+            print ("evalAEMaybe If0 ((Num 0) (Num 0) (Num 0)): ", evalAEMaybe_t18)
+
+            print "evalM test cases"
+            print ("evalM (Num 10): ", evalM_t1)
+            print ("evalM (Num -3): ", evalM_t2)
+            print ("evalM Plus ((Num 10) (Num 2)): ", evalM_t3)
+            print ("evalM Plus ((Num 10) (Num -3)): ", evalM_t4)
+            print ("evalM Minus ((Num 10) (Num 2)): ", evalM_t5)
+            print ("evalM Minus ((Num 2) (Num 10)): ", evalM_t6)
+            print ("evalM Minus ((Num 10) (Num -3)): ", evalM_t7)
+            print ("evalM Mult ((Num 10) (Num 2)): ", evalM_t8)
+            print ("evalM Mult ((Num 10) (Num -3)): ", evalM_t9)
+            print ("evalM Mult ((Num 10) (Num 0)): ", evalM_t10)
+            print ("evalM Div ((Num 10) (Num 2)): ", evalM_t11)
+            print ("evalM Div ((Num 10) (Num -3)): ", evalM_t12)
+            print ("evalM Div ((Num 10) (Num 0)): ", evalM_t13)
+            print ("evalM If0 ((Num 10) (Num 2) (Num 4)): ", evalM_t14)
+            print ("evalM If0 ((Num 0) (Num 2) (Num 4)): ", evalM_t15)
+            print ("evalM If0 ((Num 0) (Num -3) (Num 4)): ", evalM_t16)
+            print ("evalM If0 ((Num 0) (Num 0) (Num 4)): ", evalM_t17)
+            print ("evalM If0 ((Num 0) (Num 0) (Num 0)): ", evalM_t18)
+
+            print "interpAE test cases"
+            print ("1+3: ", interpAEt1)
+            print ("(1+3)*5 + 1: ", interpAEt2)
+            print ("(1+3)*5/0: ", interpAEt3)
+            print ("(1+3)*(-1)/8: ", interpAEt4)
+            print ("(1+3)*2-100: ", interpAEt5)
+            putStrLn "All test cases executed!"
+
+main = do run_test_cases
 
 
 
