f109fe6d56bdce7f55ea92fcc09b33f629df3592
Chuan Sun <telenovelachuan@gmail.com>
2020-11-13 15:46:43 -0600

    init proj 3

---
+++ EECS_662_programming_language/p3.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE GADTs #-}
+
+-- Imports for Monads
+
+import Control.Monad
+
+-- FAE AST and Type Definitions
+
+data FAE where
+  Num :: Int -> FAE
+  Plus :: FAE -> FAE -> FAE
+  Minus :: FAE -> FAE -> FAE
+  Lambda :: String -> FAE -> FAE
+  App :: FAE -> FAE -> FAE
+  Id :: String -> FAE
+  deriving (Show,Eq)
+
+type Env = [(String,FAE)]
+
+evalDynFAE :: Env -> FAE -> (Maybe FAE)
+evalDynFAE _ _ = Nothing
+
+data FAEValue where
+  NumV :: Int -> FAEValue
+  ClosureV :: String -> FAE -> Env' -> FAEValue
+  deriving (Show,Eq)
+  
+type Env' = [(String,FAEValue)]
+
+evalStatFAE :: Env' -> FAE -> (Maybe FAEValue)
+evalStatFAE _ _ = Nothing
+
+
+-- FBAE AST and Type Definitions
+
+data FBAE where
+  NumD :: Int -> FBAE
+  PlusD :: FBAE -> FBAE -> FBAE
+  MinusD :: FBAE -> FBAE -> FBAE
+  LambdaD :: String -> FBAE -> FBAE
+  AppD :: FBAE -> FBAE -> FBAE
+  BindD :: String -> FBAE -> FBAE -> FBAE
+  IdD :: String -> FBAE
+  deriving (Show,Eq)
+
+elabFBAE :: FBAE -> FAE
+elabFBAE _ = (Num (-1))
+
+evalFBAE :: Env' -> FBAE -> (Maybe FAEValue)
+evalFBAE _ _ = Nothing
+
+-- FBAEC AST and Type Definitions
+
+data FBAEC where
+  NumE :: Int -> FBAEC
+  PlusE :: FBAEC -> FBAEC -> FBAEC
+  MinusE :: FBAEC -> FBAEC -> FBAEC
+  TrueE :: FBAEC
+  FalseE :: FBAEC
+  AndE :: FBAEC -> FBAEC -> FBAEC
+  OrE :: FBAEC -> FBAEC -> FBAEC
+  NotE :: FBAEC -> FBAEC
+  IfE :: FBAEC -> FBAEC -> FBAEC -> FBAEC
+  LambdaE :: String -> FBAEC -> FBAEC
+  AppE :: FBAEC -> FBAEC -> FBAEC
+  BindE :: String -> FBAEC -> FBAEC -> FBAEC
+  IdE :: String -> FBAEC
+  deriving (Show,Eq)
+
+elabFBAEC :: FBAEC -> FAE
+elabFBAEC _ = (Num (-1))
+
+evalFBAEC :: Env' -> FBAEC -> Maybe FAEValue
+evalFBAEC _ _ = Nothing
