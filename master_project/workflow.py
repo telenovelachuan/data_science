d5fd2873504b98a2e0bf08ea6433433b5af8f464
Chuan Sun <chuansun.sc@gmail.com>
2020-11-26 17:05:49 -0600

    tinker around AL strategies

--- master_project/workflow.py
+++ master_project/workflow.py
@@ -1,5 +1,5 @@
 import os
-#os.environ["CUDA_VISIBLE_DEVICES"]="2"  # specify which GPU(s) to be used
+# os.environ["CUDA_VISIBLE_DEVICES"]="2"  # specify which GPU(s) to be used
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
@@ -24,19 +24,21 @@ from tensorflow.keras.optimizers import *
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from modAL.models import ActiveLearner
 from modAL.uncertainty import entropy_sampling, margin_sampling, uncertainty_sampling
-
+import copy
 
 CTW_labelled = "/home/c693s270/"
+
+
 def get_data(data_file):
-    
+
     f = h5py.File(data_file, 'r')
-    H_Re = f['H_Re'][:] #shape (sample size, 56, 924, 5)
-    H_Im = f['H_Im'][:] #shape (sample size, 56, 924, 5)
-    SNR = f['SNR'][:] #shape (sample size, 56, 5)
-    Pos = f['Pos'][:] #shape(sample size, 3)
+    H_Re = f['H_Re'][:]  # shape (sample size, 56, 924, 5)
+    H_Im = f['H_Im'][:]  # shape (sample size, 56, 924, 5)
+    SNR = f['SNR'][:]  # shape (sample size, 56, 5)
+    Pos = f['Pos'][:]  # shape(sample size, 3)
     f.close()
-            
-    return H_Re, H_Im, SNR, Pos        
+
+    return H_Re, H_Im, SNR, Pos
 
 # # load data from file_1.hdf4
 
@@ -65,7 +67,7 @@ def get_data(data_file):
 #             h = H[d1][d2][d3]
 #             phase = arctan[d1][d2][d3]
 #             h1[d1][d2][d3] = np.vstack((h, phase)).reshape(-1)
-        
+
 # #h1.shape
 
 # # append SNR onto h1
@@ -77,12 +79,13 @@ def get_data(data_file):
 #         h = h1[d1][d2]
 #         snr = snr_rep[d1][d2]
 #         h2[d1][d2] = np.vstack((h, snr))
-        
+
 # #h2.shape
 
 # # replace inifinity values
 # h2[h2 == inf] = 0
 # h2[h2 == -inf] = 0
+
 
 # load data from pre-saved file
 print("Loading all training data...")
@@ -131,7 +134,7 @@ with open('all_ae.npy', 'rb') as f:
 #     model.add(Dense(32, activation='relu'))
 #     model.add(Dense(8, activation='relu'))
 #     model.add(Dense(3))
-#     model.compile(loss='mean_absolute_percentage_error', optimizer=opt) 
+#     model.compile(loss='mean_absolute_percentage_error', optimizer=opt)
 #     return model
 
 def dnn_model(opt=Adam(1e-3), dropout_rate=0.2):
@@ -148,6 +151,7 @@ def dnn_model(opt=Adam(1e-3), dropout_rate=0.2):
     model.add(Dense(3))
     model.compile(loss='mean_squared_error', optimizer=opt)
     return model
+
 
 def cnn_model1(input_shape, opt=Adam(1e-3), dropout_rate=0.2):
     model = Sequential()
@@ -183,14 +187,14 @@ def cnn_model1(input_shape, opt=Adam(1e-3), dropout_rate=0.2):
 #     model.add(Conv2DTranspose(128,(3,3), strides=(1, 5), padding='same', activation='relu'))
 #     #model.add(ZeroPadding2D(((0, 0), (2, 1))))
 #     model.add(Conv2DTranspose(10, (3, 3) , strides=(1, 5), padding='same', activation='linear'))
-#     model.compile(loss='mean_squared_error', optimizer=Adam(1e-3)) 
+#     model.compile(loss='mean_squared_error', optimizer=Adam(1e-3))
 #     return model
 
 # ae = autoencoder()
 # # ae.summary()
 
 # print("Try AutoEncoder. Train-test split for AutoEncoder...")
-# data, data_v  = train_test_split(h2, test_size=0.5, random_state=54) 
+# data, data_v  = train_test_split(h2, test_size=0.5, random_state=54)
 # data.shape
 
 # print("training AutoEncoder...")
@@ -203,6 +207,7 @@ def cnn_model1(input_shape, opt=Adam(1e-3), dropout_rate=0.2):
 # h2_ae = encoder.predict(h2)
 h2_ae_shape = h2_ae.shape
 h2_ae_1dcnn = h2_ae.reshape(h2_ae_shape[0], h2_ae_shape[1], -1)
+
 
 def dnn_ae_model(opt=Adam(1e-3), dropout_rate=0.2):
     model = Sequential()
@@ -219,24 +224,31 @@ def dnn_ae_model(opt=Adam(1e-3), dropout_rate=0.2):
     model.compile(loss='mean_squared_error', optimizer=opt)
     return model
 
+
 print("train-test splitting ae data...")
-x_train_ae, x_test_ae, y_train_ae, y_test_ae = train_test_split(h2_ae, Pos, test_size=0.1, random_state=42)
-x_train_ae_cnn, x_test_ae_cnn, y_train_ae_cnn, y_test_ae_cnn = train_test_split(h2_ae_1dcnn, Pos, test_size=0.1, random_state=42)
+x_train_ae, x_test_ae, y_train_ae, y_test_ae = train_test_split(
+    h2_ae, Pos, test_size=0.1, random_state=42)
+x_train_ae_cnn, x_test_ae_cnn, y_train_ae_cnn, y_test_ae_cnn = train_test_split(
+    h2_ae_1dcnn, Pos, test_size=0.1, random_state=42)
 
 print("splitting training data and active learning pool...")
-train_x_al, pool_x_al, train_y_al, pool_y_al = train_test_split(x_train_ae_cnn, y_train_ae_cnn, test_size=0.8, random_state=42)
+train_x_al, pool_x_al, train_y_al, pool_y_al = train_test_split(
+    x_train_ae_cnn, y_train_ae_cnn, test_size=0.8, random_state=42)
+print(f"Test samples: {len(y_test_ae_cnn)}, pool size: {len(pool_y_al)}")
 
 print("constructing 1D-CNN model...")
 lr = 5e-3
 epochs = 50
 decay_rate = lr / 50
-model_ae = cnn_model1(train_x_al.shape[1:], opt=Adam(5e-3, decay=decay_rate)) 
+model_ae = cnn_model1(train_x_al.shape[1:], opt=Adam(5e-3, decay=decay_rate))
 
 
 def get_prediction_precision(regressor, x_test, y_test):
     y_pred = regressor.predict(x_test)
     mse = mean_squared_error(y_true=y_test, y_pred=y_pred)
-    print(f"current MSE: {mse}")
+    #print(f"current MSE: {mse}")
+    return mse
+
 
 def random_sampling(classifier, X_pool):
     n_samples = len(X_pool)
@@ -244,30 +256,62 @@ def random_sampling(classifier, X_pool):
     return [query_idx], X_pool[query_idx]
 
 
-n_queries = 3500
+# 1. Fit on original training data
+model_ae.fit(train_x_al, train_y_al, epochs=5, validation_data=(
+    x_test_ae_cnn, y_test_ae_cnn), verbose=1)
+print(
+    f"Initial round of training finished, MSE:{get_prediction_precision(model_ae, x_test_ae_cnn, y_test_ae_cnn)}")
+print("Begin active learning process...")
+
+n_queries = 2700
+mode = "entropy"
 #queries_num = np.linspace(100, 3500, 35)
 mse_dict = {}
-chosen_samples =[]
+all_chosen_samples = []
+mode_dict = {
+    "random": random_sampling,
+    "uncertainty": uncertainty_sampling,
+    "margin": margin_sampling,
+    "entropy": entropy_sampling
+}
 print("Initialize active learner...")
 regressor = ActiveLearner(
-            estimator=model_ae,
-            query_strategy=margin_sampling,
-            X_training=train_x_al, y_training=train_y_al
+    estimator=model_ae,
+    query_strategy=mode_dict[mode],
+    X_training=train_x_al, y_training=train_y_al
 )
 print(f"active learning for {n_queries} epochs")
+
+_chosen_x, _chosen_y = [], []
+# _new_x = copy.copy(train_x_al)
+# _new_y = copy.copy(train_y_al)
 for i in range(n_queries + 1):
 
-        # get_prediction_precision(regressor, x_test_ae_cnn, y_test_ae_cnn)
+    # get_prediction_precision(regressor, x_test_ae_cnn, y_test_ae_cnn)
     query_idx, query_instance = regressor.query(pool_x_al)
-    chosen_samples.append(pool_y_al[query_idx])
-    regressor.teach(pool_x_al[query_idx], pool_y_al[query_idx])
-    if i % 100 == 0:
-    	
-    	get_prediction_precision(regressor, x_test_ae_cnn, y_test_ae_cnn)
-    	y_pred = regressor.predict(x_test_ae_cnn)
-    	mse = mean_squared_error(y_true=y_test_ae_cnn, y_pred=y_pred)
-    	mse_dict[i] = mse
-    	print(f"Evaluating model at {i}th query...mse:{mse}")
+    _x, _y = pool_x_al[query_idx], pool_y_al[query_idx]
+    #print(f"_x:{_x}, _y:{_y}")
+    all_chosen_samples.append(_y)
+    _chosen_x.append(_x)
+    _chosen_y.append(_y)
+
+    if i % 50 == 0 and i > 0:
+        # batch mode AL teach
+        regressor.teach(_chosen_x[0], _chosen_y[0], only_new=False)
+        # _model = copy.copy(model_ae)
+        # _new_x = np.concatenate([_new_x, _chosen_x[0]])
+        # _new_y = np.concatenate([_new_y, _chosen_y[0]])
+
+        #model_ae.fit(_chosen_x[0], _chosen_y[0], validation_data=(x_test_ae_cnn, y_test_ae_cnn), verbose=1)
+
+        # _model.fit(_new_x, _new_y, epochs=10, validation_data=(
+        #     x_test_ae_cnn, y_test_ae_cnn), verbose=1)
+        mse = get_prediction_precision(regressor, x_test_ae_cnn, y_test_ae_cnn)
+        # y_pred = regressor.predict(x_test_ae_cnn)
+        # mse = mean_squared_error(y_true=y_test_ae_cnn, y_pred=y_pred)
+        mse_dict[i] = mse
+        print(f"Evaluating model at {i}th query...mse:{mse}")
+        _chosen_x, _chosen_y = [], []
 
 #print("training cnn model on ae...")
 # lr = 5e-3
@@ -280,19 +324,19 @@ for i in range(n_queries + 1):
 #                  callbacks=[earlystopper], batch_size=4, verbose=1)
 
 print("saving model...")
-model_ae.save("models/model_ae_al_margin_all.h5")
+model_ae.save(f"models/model_ae_al1_{mode}_all.h5")
 print("making predictions & saving results...")
 preds = model_ae.predict(x_test_ae_cnn)
 result = pd.DataFrame(preds, columns=["x", "y", "z"])
 print(f"{len(result)} rows")
-result.to_csv("preds/cnn_ae_al_margin_all_preds.csv")
+result.to_csv(f"preds/cnn_ae_al1_{mode}_all_preds.csv")
 print("saving chosen samples")
-df_chosen = pd.DataFrame(np.array(chosen_samples).reshape(-1, 3), columns=["x", "y", "z"])
-df_chosen.to_csv("chosen/cnn_ae_al_margin_all_chosen.csv")
-print("All done!!")
+df_chosen = pd.DataFrame(
+    np.array(all_chosen_samples).reshape(-1, 3), columns=["x", "y", "z"])
+df_chosen.to_csv(f"chosen/cnn_ae_al1_{mode}_all_chosen.csv")
+print(f"All done!! mode:{mode}")
 print(f"mse_dict: {mse_dict}")
 # print("saving model...")
 # model0.save('model3.h5')
 # print("All done!")
 #val_loss = hist.history['val_loss']
-
