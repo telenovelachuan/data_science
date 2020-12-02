b8250dcbf4585d2441104d8bf77155fdbcca19a4
Chuan Sun <chuansun.sc@gmail.com>
2020-12-01 20:08:18 -0600

    init proj 4

---
+++ EECS_662_programming_language/a4.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE GADTs #-}
+
+-- import Control.Monad
+
+-- Calculator language extended with an environment to hold defined variables
+
+data TFBAE where
+  TNum :: TFBAE
+  TBool :: TFBAE
+  (:->:) :: TFBAE -> TFBAE -> TFBAE
+  deriving (Show,Eq)
+
+data FBAE where
+  Num :: Int -> FBAE
+  Plus :: FBAE -> FBAE -> FBAE
+  Minus :: FBAE -> FBAE -> FBAE
+  Mult :: FBAE -> FBAE -> FBAE
+  Div :: FBAE -> FBAE -> FBAE
+  Bind :: String -> FBAE -> FBAE -> FBAE
+  Lambda :: String -> TFBAE -> FBAE -> FBAE
+  App :: FBAE -> FBAE -> FBAE
+  Id :: String -> FBAE
+  Boolean :: Bool -> FBAE
+  And :: FBAE -> FBAE -> FBAE
+  Or :: FBAE -> FBAE -> FBAE
+  Leq :: FBAE -> FBAE -> FBAE
+  IsZero :: FBAE -> FBAE
+  If :: FBAE -> FBAE -> FBAE -> FBAE
+  Fix :: FBAE -> FBAE
+  deriving (Show,Eq)
+
+-- Value defintion for statically scoped eval
+
+data FBAEVal where
+  NumV :: Int -> FBAEVal
+  BooleanV :: Bool -> FBAEVal
+  ClosureV :: String -> FBAE -> Env -> FBAEVal
+  deriving (Show,Eq)
+
+-- Enviornment for statically scoped eval
+
+type Env = [(String,FBAEVal)]
+
+-- Statically scoped eval
+         
+evalM :: Env -> FBAE -> (Maybe FBAEVal)
+evalM _ _ = Nothing
+
+-- Type inference function
+
+type Cont = [(String,TFBAE)]
+
+typeofM :: Cont -> FBAE -> (Maybe TFBAE)
+typeofM _ _ = Nothing
+
+
+-- Interpreter
+
+interp :: FBAE -> (Maybe FBAEVal)
+interp _ = Nothing
+
+-- Factorial function for testing evalM and typeofM.  the type of test1 should
+-- be TNum and the result of evaluating test1`should be (NumV 6).  Remember
+-- that Just is used to return both the value and type.
+
+test1 = (Bind "f" (Lambda "g" ((:->:) TNum TNum)
+                    (Lambda "x" TNum (If (IsZero (Id "x")) (Num 1)
+                                         (Mult (Id "x")
+                                               (App (Id "g")
+                                                    (Minus (Id "x")
+                                                           (Num 1)))))))
+         (App (Fix (Id "f")) (Num 3)))
